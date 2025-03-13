e1c29fa4860fc007ea7003388a2804ccf931c1df
pixy <wen.tao.cnu@gmail.com>
2025-03-13 18:00:30 +0800

    优化尺寸

--- assets/gg-media-gallery-ex.js
+++ assets/gg-media-gallery-ex.js
@@ -219,7 +219,7 @@ class MediaGalleryEx extends HTMLElement {
     // 创建返回按钮
     const backButton = document.createElement('button');
     backButton.className = 'mobile-zoom-back';
-    backButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H5M12 19l-7-7 7-7"/></svg>';
+    backButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H5M12 19l-7-7 7-7"/></svg>';
     backButton.addEventListener('click', () => this.closeZoomOverlay());
     
     // 创建图片计数指示器
